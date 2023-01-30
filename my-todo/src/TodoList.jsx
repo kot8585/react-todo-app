af697eff232ef417e803b9ec17dddfed3c8de180
kot8585 <ansgywjd321@gmail.com>
2023-01-30 19:12:19 +0900

    "my" - todo 완료 기능 개발

--- my-todo/src/TodoList.jsx
+++ my-todo/src/TodoList.jsx
@@ -5,6 +5,7 @@ import { BsFillTrashFill } from "react-icons/bs";
 
 export default function TodoList() {
   const [todoList, setTodoList] = useState([{id: '1234', text: '냥냥', isCompleted: false}]);
+  
 
   const handleAdd = (todo) => {
     setTodoList([...todoList, todo]);
@@ -12,13 +13,26 @@ export default function TodoList() {
 
   const handleDelete = (id) => {
     setTodoList(todoList.filter(todo => todo.id !== id));
-  }
+  };
+
+  const handleChecked = (id) => {
+    const todo = todoList.find((todo) => todo.id === id);
+    todo.isCompleted = !todo.isCompleted;
+
+    setTodoList([...todoList]);
+  };
 
   return (
     <section>
       <ul>
         {todoList.map((todo) => {
-          return <li key={todo.id}>{todo.text}<BsFillTrashFill onClick={() => handleDelete(todo.id)}/></li>
+          return <li key={todo.id}>
+            <input 
+            type='checkbox' 
+            value={todo.isCompleted}
+            onClick={() => handleChecked(todo.id)}/>
+          {todo.text}
+          <BsFillTrashFill onClick={() => handleDelete(todo.id)}/></li>
         })}
       </ul>
 
