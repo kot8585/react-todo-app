fde75d97ffbd17f5c5c7ec06f6a4cb2181338c30
kot8585 <ansgywjd321@gmail.com>
2023-02-01 14:58:00 +0900

    "ellie" - addTodo, TodoList 스타일링 완료
    - flex 사용
    - button hover 시 filter : brightness
    - box-sizing
    - 버튼을 동그랗게 꾸미기 위해 <button>을 <span>으로 감쌌다.
    - checkbox에 lael을 붙여서 라벨을 클릭해도 체크박스가 체크되는듯..?

--- ellie-todo-app/src/components/Todo/Todo.jsx
+++ ellie-todo-app/src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FaTrashAlt} from 'react-icons/fa'
+import styles from './Todo.module.css'
 
 export default function Todo({todo, onUpdate, onDelete}) {
   const {text, status} = todo;
@@ -10,18 +11,21 @@ export default function Todo({todo, onUpdate, onDelete}) {
   const handleDelete = () => onDelete(todo);
 
   return (
-    <li key={todo.id}>
+    <li key={todo.id} className={styles.todo}>
       <input 
+      className={styles.checkbox}
       type="checkbox" 
       id={todo.id} 
       checked={status === 'completed'}
       onChange={handleChange}
       />
       {/* 라벨 왜 붙이는거지? input의 id와 label의 htmlFor 연결. 리더기를 위해 사용?*/}
-      <label htmlFor={todo.id}>{text}</label>
-      <button onClick={handleDelete}>
+      <label htmlFor={todo.id} className={styles.text}>{text}</label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
         <FaTrashAlt/>
       </button>
+      </span>
     </li>
   );
 }
